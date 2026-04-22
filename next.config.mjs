7415ef51dd00ede168b39297eb55ba7e2d1dc8fe
Qutaiba Kaleem <qutaibakaleem@Qutaibas-MacBook-Air.local>
2026-04-22 17:36:14 -0400

    Out Folder Ready

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out', // Explicitly forcing the directory name
   images: {
     unoptimized: true,
   },
