70b3c2c13a131969bcf964042b28f9863d2f0f45
Qutaiba Kaleem <qutaibakaleem@Qutaibas-MacBook-Air.local>
2026-04-22 17:50:45 -0400

    Fix static export config and deploy path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out', // Explicitly forcing the directory name
   images: {
     unoptimized: true,
   },
